a1f3b6dadb2e55752e7eee14a5cd16ba8c69f1b3
Thiago Cruz <tlcs1993@gmail.com>
2020-02-07 23:13:49 -0300

    After class 55 "Handling PATCH Requests"

--- app.js
+++ app.js
@@ -26,23 +26,6 @@ app.get('/api/v1/tours', (req, res) => {
     })
 });
 
-app.post('/api/v1/tours', (req, res) => {
-    // console.log(req.body);
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({ id: newId }, req.body);
-
-    tours.push(newTour);
-
-    fs.writeFile(`${ __dirname }/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: newTour
-            }
-        });
-    });
-});
-
 app.get('/api/v1/tours/:id', (req, res) => {
     console.log(req.params);
 
@@ -66,6 +49,39 @@ app.get('/api/v1/tours/:id', (req, res) => {
             tours: tour
         }
     });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body);
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile(`${ __dirname }/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        });
+    });
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: '<Updated tour here!>'
+        }
+    })
 });
 
 const port = 3000;
