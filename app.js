fedbae9d8eaad565eef8636f5626b9a85d09ccce
Thiago Cruz <tlcs1993@gmail.com>
2020-02-16 19:02:08 -0300

    After class 67 "Environment Variables"

--- app.js
+++ app.js
@@ -5,26 +5,32 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+/* ***** MIDDLEWARES ***** */
+
 // Middleware that allows some properties on the request.
 app.use(express.json());
 
-// HTTP request logger middleware for node.js.
-app.use(morgan('dev'));
+// HTTP request logger middleware for node.js. Will only log if it's in the development environment.
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
-// Serving a static file (demonstration purpose).
+// Serve a static file (demonstration purpose).
 app.use(express.static(`${ __dirname }/public`));
 
 // Logs a message each time the app has made a requisition (demonstration purpose).
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
     console.log('Hello from the middleware!');
     next();
-});
+}); */
 
 // Create a variable in the req property to save the time of each request (demonstration purpose).
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
 });
+
+/* ***** ROUTES ***** */
 
 // Mount the tour routes.
 app.use('/api/v1/tours', tourRouter);
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+const dotenv = require('dotenv'); // Module that loads environment variables from a '.env' file.
+
+// Loads the variables from the '.env' file contained in the path.
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
-const port = 3000;
+// Show all the process environment variables.
+// console.log(process.env);
+
+// Choose the port set on the 'config.env' file or the default 3000.
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`App running on port ${ port }...`);
