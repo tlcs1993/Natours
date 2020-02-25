87d03beb876c1d2f17a89d897e685a31ecbecd77
Thiago Cruz <tlcs1993@gmail.com>
2020-02-25 17:48:40 -0300

    After class 84 "Creating a Simple Tour Model"

--- server.js
+++ server.js
@@ -21,6 +21,26 @@ mongoose
     })
     .then(() => console.log('Database connection successful!'));
 
+// Create the tour schema.
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true,
+    },
+    rating: {
+        type: Number,
+        default: 4.5,
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price'],
+    },
+});
+
+// Create the tour model.
+const Tour = mongoose.model('Tour', tourSchema);
+
 // Show all the process environment variables.
 // console.log(process.env);
 
