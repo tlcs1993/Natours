8247e6141f9dc90eeb94c64d961b09d453990cd1
Thiago Cruz <tlcs1993@gmail.com>
2020-02-27 00:29:56 -0300

    After class 88 "Another Way of Creating Documents"

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,17 +1,5 @@
 const Tour = require('./../models/tourModel');
 
-// Check if the request's body contains the name and the price of the tour.
-exports.checkBody = (req, res, next) => {
-    if (!req.body.name || !req.body.price) {
-        return res.status(400).json({
-            status: 'fail',
-            message: 'Missing name or price',
-        });
-    }
-
-    next();
-};
-
 // Retrieve all the tours.
 exports.getAllTours = (req, res) => {
     console.log(`Request time: ${req.requestTime}`);
@@ -45,13 +33,24 @@ exports.getTour = (req, res) => {
 };
 
 // Create a new tour.
-exports.createTour = (req, res) => {
-    res.status(201).json({
-        status: 'success',
-        // data: {
-        //     tour: newTour,
-        // },
-    });
+exports.createTour = async (req, res) => {
+    try {
+        // const newTour({});
+        // newTour.save();
+        const newTour = await Tour.create(req.body);
+
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour,
+            },
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: 'Invalid data sent!',
+        });
+    }
 };
 
 // Change the values of a specific tour based on the ID.
